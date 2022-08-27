d3b2587b3dc5be6e90bcaca29f492c5478288044
deepgoriya <110027757+deepgoriya@users.noreply.github.com>
2022-08-27 11:46:14 +0530

    Add files via upload

---
+++ Assignment/info.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("\nname- deep goriya");
+	printf("\ndate of birth- 30/12/2003");
+	printf("\nage- 19");
+	printf("\naddress- gorkhijadiya t-morbi j-morbi");
+	return 0;
+}
