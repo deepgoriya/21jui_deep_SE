dcf6208e2e545950e8dc3da83b9eab414ec21369
deepgoriya <110027757+deepgoriya@users.noreply.github.com>
2022-09-03 08:47:27 +0530

    Add files via upload

---
+++ Task/daimand shap1e.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	for(a=1;a<=5;a++)
+	{
+		for(c=5;c>a;c--)
+		{
+			printf(" ");
+		}
+		for(b=1;b<=a;b++)
+		{
+			printf("%d ",b);
+		}
+			printf("\n");
+	}
+	for(a=5;a>=1;a--)
+	{
+		for(c=5;c>a;c--)
+		{
+			printf(" ");
+		}
+		for(b=1;b<=a;b++)
+		{
+			printf("%d ",b);
+		}
+			printf("\n");
+	}
+}
+
