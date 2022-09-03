52978f52f31b764f8e04d7da6961e6bbbe211589
deepgoriya <110027757+deepgoriya@users.noreply.github.com>
2022-09-03 20:30:10 +0530

    Add files via upload

---
+++ Task/pattern A.c.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for (i=1;i<=7;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if( (i==1 && j>1 && j<5) || (j==1 && i>1) || (j==5 && i>1) || i==4)
+			{
+				printf("* ",j);
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+}
